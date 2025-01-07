9abd38b7feb3db2e4e48909826de0f5b0c8930ae
Herculles Hendrius <herculles.hendriuss@gmail.com>
2025-01-07 18:34:34 -0300

    implement cloudwatch alarms

--- lambda/orders/ordersFunction.ts
+++ lambda/orders/ordersFunction.ts
@@ -85,7 +85,7 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
                 body: JSON.stringify(convertToOrderResponse(order))
             }
         } else {
-
+            console.error('Some product was not found')
             const result = await eventBridgeClient.putEvents({
                 Entries: [
                     {
